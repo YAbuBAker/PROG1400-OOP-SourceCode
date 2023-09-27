716d773177cc4cca88102a413c88c7b1820b3ad9
W0304743 <w0304743@campus.nscc.ca>
2023-09-26 23:29:44 -0300

    New Repository Modified

--- NewProjectOneGit/src/Main.java
+++ NewProjectOneGit/src/Main.java
@@ -3,5 +3,6 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("Hi");
         System.out.println("Hello");
+        System.out.println("Hi Again");
     }
 }
